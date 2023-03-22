a3ef0d0f90f1604ec4b95aad5723d047642eab5e
Heloisa Felizardo <heloisa.felizardo@yahoo.com>
2023-03-21 23:01:08 -0300

    Refatoração vowelCount

--- vowelCount/index.js
+++ vowelCount/index.js
@@ -5,10 +5,8 @@ Consideraremos a, e, i, o, ucomo vogais para este Kata (mas não y).
 A string de entrada consistirá apenas em letras minúsculas e/ou espaços.*/
 
 const getCount = (str) => {
-	let maiusculas = str.includes('A', 'E', 'I', 'O', 'U');
-
-	return !maiusculas ? (str.match(/[aeiou]/gi) || []).length : false;
+	return (str.match(/[aeiou]/g) || []).length;
 };
 console.log(getCount('abracadabra'), 5);
-console.log(getCount('abracadabra abracadabra'), 5);
-console.log(getCount('ABRACADABRA ABRACADABRA'), 5);
+console.log(getCount('abracadabra abracadabra'), 10);
+console.log(getCount('ABRACADABRA ABRACADABRA'), 0);
